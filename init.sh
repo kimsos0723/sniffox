b3bbc6bfdadca3849f0d9b51afb14def0b4ce368
kimsos0723 <kimsos0723@gmail.com>
2019-06-08 01:06:40 +0900

    Rename snifox -> sniffox

--- init.sh
+++ init.sh
@@ -10,4 +10,4 @@ make
 make install
 sudo apt install libboost-program-options-dev
 cd ../../
-g++ -std=c++17 ./src/sslstrip/ssl.cpp ./src/arpspoof/arpspoof.cpp ./src/main.cpp -lpthread -ltins -lboost_program_options -L/usr/local/lib -o snifox
+g++ -std=c++17 ./src/sslstrip/ssl.cpp ./src/arpspoof/arpspoof.cpp ./src/main.cpp -lpthread -ltins -lboost_program_options -L/usr/local/lib -o sniffox
