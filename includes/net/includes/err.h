269b1880b52b0a0ae3809287c5f6991610f61b68
kimsos0723 <kimsos0723@gmail.com>
2019-07-24 23:02:56 +0900

    Add namespace network

--- includes/net/includes/err.h
+++ includes/net/includes/err.h
@@ -2,11 +2,15 @@
 
 #include "assets.h"
 
+namespace network {
+    
 enum class IpErr {
     NOTIP,
 };
 
 IpErr NotIpAddrString()
-{    
-    return  IpErr::NOTIP;
+{
+    return IpErr::NOTIP;
 }
+} // namespace network
+
